29b4d79df000cebe44b1c33fa9f6dfbbfbaaf636
Alexandre Tranchant <Alexandre.Tranchant@gmail.com>
2015-01-01 15:24:25 +0100

    Ajout de la boutique Amazon

---
+++ boutique/livre.php
@@ -0,0 +1,35 @@
+<?php
+/**
+*
+* @package phpBB3
+* @version $Id$
+* @copyright (c) 2005 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+/**
+*/
+
+/**
+* @ignore
+*/
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../jeuderole/';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup('boutique');
+
+// Output page
+page_header($user->lang['BOUTIQUE']);
+
+$template->set_filenames(array(
+	'body' => 'boutique/livre.html')
+);
+
+page_footer();
+
+?>
---
+++ jeuderole/language/fr/boutique.php
@@ -0,0 +1,50 @@
+<?php
+/** 
+*
+* boutique [Standard french]
+* translated originally by PhpBB-fr.com <http://www.phpbb-fr.com/> and phpBB.biz <http://www.phpBB.biz>
+*
+* @package language
+* @version $Id: boutique.php, v1.25 2009/12/16 16:36:00 Elglobo Exp $
+* @copyright (c) 2005 phpBB Group 
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+   $lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+
+$lang = array_merge($lang, array(
+	'BOUTIQUE'	        => 'La boutique de 2666',
+	'BIBLIOTHEQUE'	    => 'Notre bibliothèque',
+	'BOOKMARK_ARCHESS'	=> 'La bibliothèque d’Archess',
+	'BOOKMARK_HARAHEL'	=> 'La bibliothèque d’Harahel',
+	'AND_YOU'		    => 'Si vous aussi vous voulez recommander quelques livres, surtout n’hésitez pas à nous en faire part dans la rubrique partenariat. J’ajouterai votre librairie.',
+));
+
+?>
